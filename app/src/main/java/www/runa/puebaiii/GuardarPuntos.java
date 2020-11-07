773fa6ddbc0485ac5a9b4c3482fb6725ee897818
mtrigoz <70357190+mtrigoz@users.noreply.github.com>
2020-11-07 17:18:17 -0300

    Actualizacion.1

--- app/src/main/java/www/runa/puebaiii/GuardarPuntos.java
+++ app/src/main/java/www/runa/puebaiii/GuardarPuntos.java
@@ -10,5 +10,8 @@ public class GuardarPuntos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guardar_puntos);
+
+
+
     }
 }
